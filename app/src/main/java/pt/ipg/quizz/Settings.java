dc2aa48f786dda64780e64d0994919677dc9d9f4
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2019-03-27 18:27:16 +0000

    Validação de Dados, com o Toast final ao guardar o jogador.

--- app/src/main/java/pt/ipg/quizz/Settings.java
+++ app/src/main/java/pt/ipg/quizz/Settings.java
@@ -3,6 +3,10 @@ package pt.ipg.quizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +14,42 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        Button buttonCancelar = findViewById(R.id.buttonCancelar);
+        buttonCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+        Button buttonGuardar = findViewById(R.id.buttonGuardar);
+        buttonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextNome = findViewById(R.id.editTextNome);
+                String mensagem = editTextNome.getText().toString();
+
+                if (mensagem.trim().length() == 0) {
+                    editTextNome.setError(getString(R.string.nome_obrig));
+                }else if (!mensagem.matches("[a-zA-Z ]+")){
+                    editTextNome.setError(getString(R.string.charact));
+                }else{
+                    finish();
+                    Toast.makeText(Settings.this,"Jogador Guardado",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }

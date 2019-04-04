6821871b1e31f8718095b1a0f676dc62def28f2b
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2019-04-04 23:07:24 +0100

    Criações dos botões eliminar e cancelar com um Toast

--- app/src/main/java/pt/ipg/quizz/Definic.java
+++ app/src/main/java/pt/ipg/quizz/Definic.java
@@ -3,6 +3,9 @@ package pt.ipg.quizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Definic extends AppCompatActivity {
 
@@ -10,6 +13,27 @@ public class Definic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definic);
+
+
+        Button buttonCancelar2 = findViewById(R.id.buttonCancelar2);
+        buttonCancelar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+
+        Button buttonEliminar2 = findViewById(R.id.buttonEliminar2);
+        buttonEliminar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Toast.makeText(Definic.this,"Conteúdo Apagado",Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
 
     }
